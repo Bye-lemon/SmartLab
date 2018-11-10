0abad0e8fc0e0beb47718a1ed59fb0afc794a6c7
Bye-lemon <bye_lemon@mail.dlut.edu.cn>
2018-11-10 10:38:04 +0800

    v1.1.6 Version Api Added Package Size

--- app/api/views.py
+++ app/api/views.py
@@ -40,12 +40,16 @@ def get_banner_content():
 
 @blueprint.route("/v1.0/version", methods=["GET"])
 def get_aversion_newest():
-    file = open(os.path.join(os.path.join(current_app.root_path, 'static/package'), "newest.txt"), "r", encoding="utf8")
+    dir_path = os.path.join(current_app.root_path, 'static/package')
+    file = open(os.path.join(dir_path, "newest.txt"), "r", encoding="utf8")
     newest = ["SmartLab"]
-    for str in file.readline().replace('\n', '').split(' '):
-        newest.append(str)
+    for string in file.readline().replace('\n', '').split(' '):
+        newest.append(string)
     apk_name = '_'.join(newest) + '.apk'
     apk_desc = file.readline()
     apk_path = "http://140.143.186.223/static/package/" + apk_name
+    apk_byte = os.path.getsize(os.path.join(dir_path, apk_name))
+    apk_size = str(round((float(apk_byte) / (1024 * 1024)), 2)) + ' MB'
     return jsonify(
-        dict(success=True, msg='', version_name=newest[1], version_code=int(newest[2]), desc=apk_desc, link=apk_path))
+        dict(success=True, msg='', version_name=newest[1], version_code=int(newest[2]), size=apk_size, desc=apk_desc,
+             link=apk_path))

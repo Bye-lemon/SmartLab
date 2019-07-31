475dcdbd51450b8cedc665b42cad3f30348d6d7e
Li Yingping <Bye_lemon@mail.dlut.edu.cn>
2019-07-31 21:40:44 +0800

    v1.2.1 Use PyMySQL

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class BaseConfig(object):
     BABEL_DEFAULT_LOCALE = 'zh_CN'
-    SQLALCHEMY_DATABASE_URI = r'mysql://root:password@140.143.186.223:3306/GeZhiSpace'
+    SQLALCHEMY_DATABASE_URI = r'mysql+pymysql://root:password@140.143.186.223:3306/GeZhiSpace'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAX_CONTENT_LENGTH = 20 * 1024 * 1024
 
